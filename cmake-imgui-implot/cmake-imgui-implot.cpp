2461c54e92bd924085207252e4e540d5524d7845
joao cunha <gasparcunha19@outlook.pt>
2023-04-21 15:39:18 +0100

    cmake files and project sources

---
+++ cmake-imgui-implot/cmake-imgui-implot.cpp
@@ -0,0 +1,12 @@
+﻿// cmake-imgui-implot.cpp : Defines the entry point for the application.
+//
+
+#include "cmake-imgui-implot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ cmake-imgui-implot/cmake-imgui-implot.h
@@ -0,0 +1,8 @@
+﻿// cmake-imgui-implot.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
